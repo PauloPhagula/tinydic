8198281301ba7ea7be8dd043e592673a2eb3f884
Paulo Phagula <paulo.phagula@gmail.com>
2018-05-13 18:02:37 +0200

    bump version and package for new release

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 # coding: utf-8
 
-from setuptools import setup
+from __future__ import unicode_literals, print_function
+import os
+import sys
+import codecs
+from email.utils import parseaddr
+from setuptools import setup, find_packages
+
+def file_get_contents(filename):
+    """Reads an entire file into a string."""
+    assert os.path.exists(filename) and os.path.isfile(filename), 'invalid filename: ' + filename
+    return codecs.open(filename, 'r', 'utf-8').read()
+
+SETUP_DIR = os.path.abspath(os.path.dirname(__file__))
+LONG_DESCRIPTION = '\n'.join([file_get_contents('README.md')])
 
 setup(
     name='TinyDIC',
 
-    version='0.1.0',
+    version='0.1.1',
 
     description='Tiny Python Dependency Injection Container.',
-    # long_description=__doc__,
+    long_description=LONG_DESCRIPTION,
 
     url='https://github.com/dareenzo/tinydic',
 
